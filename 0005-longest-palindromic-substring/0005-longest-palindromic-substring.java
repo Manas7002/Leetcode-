55830c85241e4a5da8a79b3e85d9ee5dbbaa9467
SRIRAM SIVA MANAS <sivamanassriram@gmail.com>
2026-08-23 23:15:09 +0530

    Time: 21 ms (35.39%), Space: 46.7 MB (31.74%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        String best = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            // ---- ODD LENGTH CHECK (single character center) ----
+            int left = i;
+            int right = i;
+
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                left = left - 1;
+                right = right + 1;
+            }
+            
+            String oddPalindrome = s.substring(left + 1, right);
+
+            if (oddPalindrome.length() > best.length()) {
+                best = oddPalindrome;
+            }
+
+            
+            left = i;
+            right = i + 1;
+
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                left = left - 1;
+                right = right + 1;
+            }
+            String evenPalindrome = s.substring(left + 1, right);
+
+            if (evenPalindrome.length() > best.length()) {
+                best = evenPalindrome;
+            }
+        }
+
+        return best;
+    }
+}
